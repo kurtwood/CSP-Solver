f2ea7162f671bd2d3aefde86a98f030955772815
kurtwood <lie.braathen@gmail.com>
2014-04-29 23:34:00 +0200

    starting on the board and constraints

---
+++ src/Board.java
@@ -0,0 +1,16 @@
+
+public class Board {
+	int[] line;
+	
+	Board(int[] line) {
+		this.line = line;
+	}
+	
+	private void drawBoard(int[] line) {
+		
+	}
+	
+	public void checkConstraints() {
+		
+	}	
+}
--- src/ScannerReadFile.java
+++ src/ScannerReadFile.java
@@ -19,15 +19,33 @@ public class ScannerReadFile {
 
     		while (scanner.hasNextLine()) {
     			String line = scanner.nextLine();
-    			System.out.println(line);
-    			//TODO: each line to solver
+    			//System.out.println(line);
+    			Board board = new Board(splitLine(line));
+    			board.checkConstraints();
     		}
     		scanner.close();
     	} catch (FileNotFoundException e) {
     		e.printStackTrace();
     	}
 	}
+	
+	public int[] splitLine(String line) {
+		int[] board = new int[81];
+		char[] temp = new char[81];
+		
+		temp = line.toCharArray();
+		
+		for (int i = 0; i < temp.length; i++) {
+			if (temp[i] == '.') {
+				board[i] = 0;
+			} else {	
+				board[i] = Character.getNumericValue(temp[i]);
+			}
+			//System.out.print(board[i]);
+		} //System.out.println(" ");
+		
+		return board;
+	}
 }
-
 
 
