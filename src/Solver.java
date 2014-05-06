05c76856a2ea76f012b940150591595e53f79d0e
kurtwood <lie.braathen@gmail.com>
2014-05-06 16:18:37 +0200

    started advancedSolver

--- src/Solver.java
+++ src/Solver.java
@@ -4,8 +4,42 @@
 //Backtracking and arc consistency/something else
 public class Solver {
 	
+	boolean solved = false;
 	
-	public void advancedSolver(Board board) {
+	//implementing arc consistency
+	public void advancedSolver (Board board) {
+		//for each cell, try each number based
+		//on the domain...
+		System.out.println("heis");
+		
+		while (!solved && !valid(board) && !constraintCheck(board)) {
+			Board split = split(board);
+			advancedSolver(split);
+			
+		}
+		if (constraintCheck(board)) {
+			solved = true;
+			writeSolution(board);
+		}
+	}
+	
+	private boolean constraintCheck(Board board) {
+		return (/*board.CheckDomains() &&*/ 
+				board.verifyRows() && 
+				board.verifyColumns() && 
+				board.verifyRegions());	}
+	
+	private boolean valid(Board board) {
+		return false;
+	}
+	
+	private Board split(Board board) {
+		return board;
+	}
+	
+	private void writeSolution(Board board) {
 		
 	}
 }
+
+
